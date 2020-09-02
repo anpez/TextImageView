4c2a71e70c342ac43c8928701c677f61bb7d641e
Antonio Nicolás Pina <antonio@yaikostudio.com>
2020-09-02 18:10:05 +0200

    Missing fields.

--- textimageview/src/main/java/com/antonionicolaspina/textimageview/Text.kt
+++ textimageview/src/main/java/com/antonionicolaspina/textimageview/Text.kt
@@ -1,9 +1,13 @@
 package com.antonionicolaspina.textimageview
 
 import android.graphics.PointF
+import android.graphics.Typeface
+import androidx.annotation.ColorInt
 
 data class Text(
     val text: String,
+    @ColorInt val textColor: Int,
+    val typeface: Typeface?,
     val position: PointF = PointF(),
     var scaleFactor: Float = 1f,
     var rotationDegress: Float = 0f
